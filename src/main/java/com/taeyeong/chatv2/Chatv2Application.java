a40251d7ecd02379f3857adf5ae7b493680db6bf
dev-taeyeong <dev.taeyeong@gmail.com>
2022-08-14 18:50:25 +0900

    first commit

---
+++ src/main/java/com/taeyeong/chatv2/Chatv2Application.java
@@ -0,0 +1,13 @@
+package com.taeyeong.chatv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatv2Application.class, args);
+	}
+
+}
